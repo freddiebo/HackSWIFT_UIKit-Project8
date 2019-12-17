47cd80e5532c79f9c8bd1e95be9c235f615194f1
VBondarev <freddiebo@yandex.ru>
2019-12-17 13:55:37 +0300

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by  Vladislav Bondarev on 17.12.2019.
+//  Copyright © 2019 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
